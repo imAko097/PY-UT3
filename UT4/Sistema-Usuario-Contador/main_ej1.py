5de21a21cb8ab6dac7fbdf34a4213158fcf46e2f
Acoidán Betancor Arocha <acoarocha097@gmail.com>
2025-03-20 18:39:53 +0000

    class Usuario

--- UT4/Sistema-Usuario-Contador/main_ej1.py
+++ UT4/Sistema-Usuario-Contador/main_ej1.py
@@ -1,4 +1,7 @@
 from contador import Contador
+from usuario import Usuario
+
+print("> Testing Contador class...")
 
 # Object
 contador = Contador(10)
@@ -8,3 +11,12 @@ contador2 = Contador(contador)
 contador.printContador()
 contador2.printContador()
 
+print("> Testing Usuario class...")
+
+# User
+usuario_test = Usuario("test")
+for i in range(10):
+    usuario_test.access()
+
+# Print the user's statistics
+usuario_test.printStatistics()
