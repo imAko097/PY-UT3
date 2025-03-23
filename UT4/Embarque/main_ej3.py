d2fe04feef00d3f37b41dae037983c6d6b5c0945
Acoidán Betancor Arocha <acoarocha097@gmail.com>
2025-03-23 19:57:50 +0000

    Ferry class

--- UT4/Embarque/main_ej3.py
+++ UT4/Embarque/main_ej3.py
@@ -1,9 +1,56 @@
 from pasajero import Passenger
+from ferry import Ferry
 
-# Create a passenger with a negative age
-p = Passenger("John", 30)
+p1 = Passenger("John", 30)
 p2 = Passenger("Mary", 25)
+# p3 = Passenger("Peter", -1)
+p4 = Passenger("Alice", 47)
 
-# Compare ages
-print(p.__lt__(p2))
-print(p2.__lt__(p))
+# Print the passengers
+print(p1)
+print(p2)
+
+print('---')
+
+# Ferry
+f = Ferry()
+
+# Add the passengers to the ferry
+f.add_passenger(p1)
+f.add_passenger(p2)
+
+# Add the passengers with the name
+f.add_passenger_with_name('Francis')
+
+# Remove the passenger
+p_removed = f.remove_passenger()
+print(f"The passenger removed:\n{p_removed}")
+
+print('---')
+
+print(f)
+
+print('---')
+
+# Another passenger removed
+p_removed = f.remove_passenger()
+print(f"The passenger removed:\n{p_removed}")
+
+print('---')
+
+print(f)
+
+print('---')
+
+f.add_passenger(p4)
+print(f)
+
+# The youngest of the ferry
+f.add_passenger(p2)
+
+print(f)
+
+print('---')
+
+the_youngest = f.get_youngest()
+print(f"The youngest of the ferry is:\n{the_youngest}")
